53cef7ee2bd14412fcf597d051122c3d0e2b6065
lvd536 <nbikiti@gmail.com>
2026-03-22 20:56:34 +0400

    - Add new deps & color samples

---
+++ components/MarketDiscovery.tsx
@@ -0,0 +1,5 @@
+export default function MarketDiscovery() {
+    return <div className="w-full h-full flex flex-col">
+        <div className="flex flex-col items-start gap-2"></div>
+    </div>;
+}
